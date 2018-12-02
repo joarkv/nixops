1c7f26ca830bc5164c92c0b1e58ad16e64eb5479
Stig Palmquist <stig@stig.io>
2018-12-02 16:25:18 +0100

    move networking config to net.nix

---
+++ nux/net.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+{
+  networking.hostName = "nux";
+
+  networking.interfaces.ens18 = {
+     ipv4.addresses = [ {
+       address = "185.35.202.197";
+       prefixLength = 26;
+     } ];
+     ipv6.addresses = [ {
+       address = "2a02:ed06::197";
+       prefixLength = 64;
+     } ];
+  };
+
+  networking.defaultGateway = "185.35.202.193";
+  networking.defaultGateway6.address = "2a02:ed06::1";
+  networking.nameservers =  ["1.1.1.1"];
+}
