b7a3ff64f9a6872648f00177f703074b48e534c9
Stig Palmquist <stig@stig.io>
2018-12-02 05:15:23 +0100

    enable mosh on nux

--- nux/configuration.nix
+++ nux/configuration.nix
@@ -16,6 +16,7 @@
      wget vim nettools tmux screen weechat nix
   ];
 
+  programs.mosh.enable = true;
   services.openssh.enable = true;
   services.openssh.permitRootLogin = "yes";
   networking.firewall.enable = true;
